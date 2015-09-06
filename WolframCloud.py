9bbcaca7f54f397dc25676b8655aae7fc66f0226
Sean Bae (Kyung Mok Bae) <s@seanbae.net>
2015-09-06 00:56:27 -0400

    ImageIdentify now displays confidence level

--- WolframCloud.py
+++ WolframCloud.py
@@ -5,7 +5,11 @@ class WolframCloud:
 
     def wolfram_cloud_call(self, **args):
         arguments = dict([(key, arg) for key, arg in args.iteritems()])
-        result = urlopen("http://www.wolframcloud.com/objects/d6286319-3ac5-4c34-91d8-e04a250f1843", urlencode(arguments))
+        result = urlopen("http://www.wolframcloud.com/objects/1da937e5-1080-4b72-b9b8-f9ffd193dc6a", urlencode(arguments))
+
+
+#http://www.wolframcloud.com/objects/d6286319-3ac5-4c34-91d8-e04a250f1843
+
         return result.read()
 
     def call(self, url):
@@ -13,25 +17,29 @@ class WolframCloud:
         return textresult
 
 def parseResult(rawResult):
-	return rawResult[19:rawResult.index("::")]
+	truncate = rawResult.index("->")+3
+
+	return (rawResult[21:rawResult.index("::")],rawResult[truncate:truncate+4])
 
 def process(imgURL):
 	cloud = WolframCloud()
-	result = ""
+	result = None
 
 	try:
-		result = "A " + parseResult(cloud.call(imgURL)) + " is in your vicinity."
+		result = parseResult(cloud.call(imgURL))
 	except:
 		result = "Failed to identify the image."
 
 	return result
 
+def displayResult(result):
+	return "You're facing a " + result[0] + " with " + result[1] + " probability."
+
 def main():
 	url = "http://kindersay.com/files/images/soccer-ball.png"
 
-	print process(url)
-
-
+	result = process(url)
+	print displayResult(result)
 
 if __name__ == '__main__':
 	main()
