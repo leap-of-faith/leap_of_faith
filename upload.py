99dd725c0c763d56a73347ea289058eb66945f30
Colin King <colin.king.96@gmail.com>
2015-09-06 07:59:04 -0400

    Added face analysis

--- upload.py
+++ upload.py
@@ -5,6 +5,7 @@ import json
 from urllib import urlencode
 from urllib2 import urlopen
 from random import random
+import unicodedata
 # import wolfram
 
 # Make sure to set your Wolfram App ID in the environment
@@ -22,6 +23,7 @@ class ImageData:
 		self.tags = []
 		self.object = None
 		self.object_perc = None
+		self.imageFaces = None
 
 	def __str__(self):
 		s = '\nImage: ' + self.img_location + ' is a ' + self.object + ' at ' + str(float(self.object_perc) * 100) + '% via ' + self.api + '.\n\n'
@@ -32,6 +34,10 @@ class ImageData:
 		for tag in self.tags:
 			s += tag['text'] + ' : ' + tag['score']
 		s += '\n'
+
+		if self.imageFaces != None:
+			s += "Gender: " + self.imageFaces["gender"].lower() + ", Age: " + self.imageFaces["age_min"] + " to " + self.imageFaces["age_max"] + "\n";
+
 		return s
 
 # Takes in a local location in the file system
@@ -47,13 +53,13 @@ def processImage(local_location):
 		# Now process this image with Alchemy
 		image_data = alchemy_processImage(local_location, type='image')
 		# Check if we need to get Wolfram data
-		if(image_data == None or float(image_data.object_perc) < ALCHEMY_MIN_PERC):
-			print 'Fetching data from Wolfram (', image_data.object_perc if image_data != None else 'None', ')'
-			remote_url_location = getRemoteUrl(local_location);
-			wolfram_image_data = wolfram_processImage(remote_url_location)
-			if(wolfram_image_data != None):
-				if(image_data == None or float(image_data.object_perc) > float(wolfram_image_data.object_perc)): 
-					image_data = wolfram_image_data
+		# if(image_data == None or float(image_data.object_perc) < ALCHEMY_MIN_PERC):
+		# 	print 'Fetching data from Wolfram (', image_data.object_perc if image_data != None else 'None', ')'
+		# 	remote_url_location = getRemoteUrl(local_location);
+		# 	wolfram_image_data = wolfram_processImage(remote_url_location)
+		# 	if(wolfram_image_data != None):
+		# 		if(image_data == None or float(image_data.object_perc) > float(wolfram_image_data.object_perc)): 
+		# 			image_data = wolfram_image_data
 		
 		# Fetch face data if there is a person in the image
 		if(image_data != None and image_data.object.lower() == "person"):
@@ -65,17 +71,28 @@ def processImage(local_location):
 # Returns a string
 def tagToSentence(image_data):
 	text = image_data.object;
-	sentences = ['A {0} is in your proximity.',
-				 'Holy moly, is that a {0}?',
-				 'I spy a {0}.',
-				 'That looks like a positively wonderful {0}',
-				 'That would be a {0}.',
-				 'That looks like a tiger. Oh sorry, that is a {0}. My bad.',
-				 ''];
-	if(image_data.object == None):
-		sentences = ["What an incredible...thing.", "That's a thing, for sure.", "One thing. Right ahead.", "I'm not answering your questions, you locked me in this bag."]
-	index = int(round(random() * (len(sentences) - 1)))
-	return sentences[index].format(str(text));
+	if(len(image_data.imageFaces) > 0):
+		# It is a person and we have a face
+		
+		sentences = [];
+		if(image_data.imageFaces["gender"].lower() == "male"):
+			sentences = ['There\'s a man in front of you. He is between the ages of {0} and {1}.']
+		else:
+			sentences = ['There\'s a woman in front of you. She is between the ages of {0} and {1}.']
+
+		index = int(round(random() * (len(sentences) - 1)))
+		return sentences[index].format(str(image_data.imageFaces["age_min"]), str(image_data.imageFaces["age_max"]));
+	else:
+		sentences = ['A {0} is in your proximity.',
+					 'Holy moly, is that a {0}?',
+					 'I spy a {0}.',
+					 'That looks like a positively wonderful {0}',
+					 'That would be a {0}.',
+					 'That looks like a tiger. Oh sorry, that is a {0}. My bad.'];
+		if(image_data.object == None):
+			sentences = ["What an incredible...thing.", "That's a thing, for sure.", "One thing. Right ahead.", "I'm not answering your questions, you locked me in this bag."]
+		index = int(round(random() * (len(sentences) - 1)))
+		return sentences[index].format(str(text));
 
 # Processes an image using the Alchemy API
 # Type can be image if the image is local
@@ -102,6 +119,26 @@ def alchemy_processImage(img_location, type = 'url'):
 # information on the age and gender of the person
 # Returns a dictionary of data
 def analyze_face(image_data):
+	response = AlchemyAPI().faceTagging('image', image_data.img_location)
+
+	if response['status'] == 'OK':
+		if(len(response['imageFaces']) > 0):
+			print response['imageFaces'];
+			age_range = response['imageFaces'][0]['age']['ageRange']
+			age_range = unicodedata.normalize('NFKD', age_range).encode('ascii', 'ignore')
+			age_min = 0
+			age_max = 100
+			if('-' in age_range):
+				age_min = age_range[0:age_range.index('-')]
+				age_max = age_range[age_range.index('-') + 1:]
+			elif('<' in age_range):
+				age_min = 0
+				age_max = age_range[age_range.index('<') + 1:]
+			image_data.imageFaces = {
+				"gender": response['imageFaces'][0]['gender']['gender'],
+				"age_min": age_min,
+				"age_max": age_max
+			}
 	return image_data
 
 
@@ -147,11 +184,13 @@ def wolfram_processImage(img_location):
 # Takes a location of the image as a URL
 def imageToText(img_location):
 	image_data = processImage(img_location)
+	print image_data
 	sentence = tagToSentence(image_data)
+	print sentence
 	return sentence
 
 
 if __name__ == '__main__':
-	imageToText('http://leapoffaith.mybluemix.net/img/boy.jpg')
+	imageToText('./public/img/sean.jpg')
 
 
